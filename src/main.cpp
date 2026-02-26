08df075e26ef098b3c248a3decfb9c4ebe92a56e
Ryman Hashem <rh805@cantab.ac.uk>
2026-02-26 02:02:31 +0000

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,36 @@
 #include <Arduino.h>
 #include <Adafruit_TinyUSB.h>
 #include "sensor.h"
+#include "ble_service.h"
 
 AngleSensor sensor(A0, 330000.0f);
+AngleBleService ble;
 
 void setup() {
+
     Serial.begin(921600);
     while (!Serial) delay(10);
 
+    Serial.println("XIAO BLE Angle Sensor");
+
     sensor.begin();
+    ble.begin();
 }
 
 void loop() {
+
     sensor.update();
 
     float angle = sensor.getAngleDeg();
     float resistance = sensor.getResistanceKOhm();
 
+    // Serial output
     Serial.print(angle, 3);
     Serial.print(", ");
     Serial.println(resistance, 4);
 
-    delay(10);
+    // BLE update
+    ble.update(angle);
+
+    delay(50);  // 20Hz update
 }
